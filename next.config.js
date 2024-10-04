42cf4cc528db80280a2cbbbe7c44f7c62d1af442
Daniel Fleming <daniel11ftw@gmail.com>
2024-10-04 10:20:20 -0400

    Modified next config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/spotify-insights',
+  assetPrefix: '/spotify-insights',
 };
 
 module.exports = nextConfig;
