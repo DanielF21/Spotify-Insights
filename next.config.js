dde4d99c8a06abffdcc3423fd83774f10a4ac15d
Daniel Fleming <daniel11ftw@gmail.com>
2024-10-04 10:27:15 -0400

    reverted next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/spotify-insights',
-  assetPrefix: '/spotify-insights',
 };
 
 module.exports = nextConfig;
